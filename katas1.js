12fd910f83b57f03d883201afde7e7f6eab3a859
lissandro <lissandroperf7@gmail.com>
2021-08-10 16:02:51 -0300

    Exercicio Corrigido, Esta Certo Desta vez

--- katas1.js
+++ katas1.js
@@ -5,162 +5,161 @@ function oneThroughTwenty() {
 
   for (let inicio = 1 ; inicio <= 20; inicio++){
 
-    
-
-    console.log(inicio)
-
+      volta.push(inicio)
     
   }
-   
+  return volta;
 }
 
 console.log(oneThroughTwenty());
 
 function evensToTwenty() {
-  let i = 1;
-  while(i<=20){
-
-    if(i%2 == 0){
-      console.log(i);
-    }
-    i++;
-  }
-
-  }
   
+  let armazenar = [];
+  for(let c = 1; c<=20; c++){
+  if(c % 2 === 0){
 
+    armazenar.push(c);
 
+    }
+    
+  }
+  return armazenar;
+}
+
+ 
+  
 console.log(evensToTwenty());
 
 function oddsToTwenty() {
     
-  let c = 1;
-  while (c<=20){
-    if(c%2 == 1 ){
-      console.log(c)
+    let armazenar = [];
 
-    }
-    c++;
+    for(let c = 1;c <= 20; c++){
+    if(c%2 == 1 ){
+      
+      armazenar.push(c)
+
+    
+    
 
   }
-   
+  
+}
+return armazenar;
 }
 
 console.log(oddsToTwenty());
 
 function multiplesOfFive() {
     
-let i =1;
-while(i<=100){
+    let armazenar = [];
 
-  if(i%5 == 0){
+    for(let c = 1 ;c <= 100 ;c++){
+    if(c%5 == 0){
 
-    console.log(i)
+    armazenar.push(c)
 
   }
 
-  i++;
+  
 }
-
+return armazenar;
 }
 
 console.log(multiplesOfFive());
 
 function squareNumbers() {
 
-    let perf = [1,4,9,16,25,36,49,64,81,100]
+    let perf = [];
 
-    let c = 0;
-    while(c <= perf.length){
+    for (let c = 1 ; c<=10 ;c ++) {
 
-      console.log(perf[c]);
-      c++;
+        perf.push(c * c)
+    } 
+
+  
+
+      return perf;
+      
     }
   
 
  // So não sei se esta correto mas foi a respota mais proxima que consegui fazer
 
   
-    
-}
-
 console.log(squareNumbers());
 
 function countingBackwards() {
     
   let inicio = [];
-  for (let i = 20 ; i >= 1 ; i--){
+  for (let c = 20 ; c >= 1 ; c--){
 
-    console.log(i);
+    inicio.push(c);
   }
-  
+  return inicio
 }
 
 console.log(countingBackwards());
 
 function evenNumbersBackwards() {
  
-    let i = 20;
-    while(i>=1){
+    let armazenar = [];
 
-      if(i%2 == 0){
+    for(let c = 20 ; c >=1 ; c--){
+      if(c % 2 === 0){
+      
+      armazenar.push(c);
 
-        console.log(i)
-      }
-        i--;
     }
- 
-}
+  }
+    return armazenar;
+  }
 
 console.log(evenNumbersBackwards());
 
 function oddNumbersBackwards() {
     
-        let i = 20;
-        while(i>=1){
+        let armazenar = [];
 
-          if(i%2 == 1){
+        for(let c = 20; c >= 1; c--){
+       
 
-            console.log(i)
+          if(c % 2 === 1){
+
+            armazenar.push(c)
           }
-
-          i--;
         }
+        return armazenar;
 }
 
   console.log(oddNumbersBackwards());
 
 function multiplesOfFiveBackwards() {
     
-    let i = 100;
-    while(i>=1){
-      if(i%5 == 0){
-        console.log(i);
+    let armazenar = [];
+    for(let c = 100; c>=5; c--){
+      if(c % 5 === 0){
+        armazenar.push(c);
+
       }
-      i--
+
     }
-
-
-
-}
+    return armazenar;
+  }
 
 console.log(multiplesOfFiveBackwards());
 
 function squareNumbersBackwards() {
-    
-      let quadrado = [100,81,64,49,36,25,16,9,4,1]
+      let armazenar = [];
 
-      let i = 0
+      for(let c = 10 ;c >=1; c--){
+        armazenar.push(c * c);
 
-      while(i <= quadrado.length){
-
-            console.log(quadrado[i]);
-            i++;        
       }
 
+      return armazenar;
 
+    }
 
-}
-
-console.log(squareNumbersBackwards());
-
+    console.log(squareNumbersBackwards());
